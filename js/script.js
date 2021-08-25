f85e8fff2cb89322742b642bedd6200c79f2e0d4
alessandroNessi <alessandronessi@msn.com>
2021-08-25 12:43:06 +0200

    aggiunta uscita

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var invitati = ["marco", "gianni", "giorgio", "giovanni", "luca", "samuele"];
 var userName = prompt("benvenuto alla festa de grande Gatsby, inserisci il tuo nome per validare l'ingresso: ");
-for( var i=0; i< invitati.length;i++){
+for( var i=0; i< invitati.length && userName!=true;i++){
     if(userName==invitati[i]){
         alert("Benvenuto alla festa");
         userName=true;
